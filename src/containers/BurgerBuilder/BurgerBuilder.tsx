804448f9b83c44c3eaf31ffc45f49ad2002a2515
baadillahnabil <nabilzero27@gmail.com>
2018-12-16 01:40:44 +0700

    add burger builder container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class BurgerBuilder extends Component {
+  constructor(props: any) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <>
+        <div>Burger</div>
+        <div>Build Control</div>
+      </>
+    )
+  }
+}
+
+export default BurgerBuilder
