4eeb998a30cb85109df20b76670a7ff2bc42a997
baadillahnabil <nabilzero27@gmail.com>
2018-12-17 01:28:30 +0700

    add burger component

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { FunctionComponent, memo } from 'react'
+import { withDefaultProps } from '../../hoc/withDefaultProps'
+import Classes from './Burger.module.sass'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = (props: any) => (
-  <>
-    <p>Burger</p>
-  </>
-)
+const defaultProps = {}
+type DefaultProps = typeof defaultProps
+type Props = {} & DefaultProps
 
-export default burger
+const burger: FunctionComponent<Props> = props => {
+  return (
+    <div className={Classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="bacon" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  )
+}
+
+export default withDefaultProps(defaultProps, memo(burger))
