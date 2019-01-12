3edb7ab20f500e4292dabb83b52e413cb606e948
baadillahnabil <nabilzero27@gmail.com>
2019-01-12 16:02:04 +0700

    add ingredients props. change ingredients to get from props

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -5,17 +5,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const defaultProps = {}
 type DefaultProps = typeof defaultProps
-type Props = {} & DefaultProps
+type Props = {
+  ingredients: Object
+} & DefaultProps
 
 const burger: FunctionComponent<Props> = props => {
+  const arrayIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+    return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+      return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+    })
+  })
+
   return (
     <div className={Classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="bacon" />
-      <BurgerIngredient type="cheese" />
+      {arrayIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   )
